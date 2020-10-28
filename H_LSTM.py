ac9d3d8875e7560a8b8583bf7576abd44b3480f7
demiludan <lud1@ornl.gov>
2020-10-28 09:08:43 -0400

    Update H_LSTM.py

--- H_LSTM.py
+++ H_LSTM.py
@@ -23,7 +23,7 @@ import time
 #----------------------------------------------------------
 #---- multivariate LSTM forecasting  ----
 #----------------------------------------------------------
-# predicting NEE for furture n_out time steps given meteorogical data at previous n_in time step.
+# predicting streamflow for furture n_out time steps given meteorogical data at previous n_in time step.
 # --- convert series to supervised learning
 def series_to_supervised(data_in, data_out, n_in=1, n_out=1, dropnan=True):
 	n_vars_in = 1 if type(data_in) is list else data_in.shape[1]
@@ -84,7 +84,7 @@ yobs_test = XYdata[-Ntest:, -nfuture]
 print('shape of yobs_train and yobs_test is ', yobs_train.shape, yobs_test.shape)
 print('min and max of yobs_test', np.min(yobs_test), np.max(yobs_test))
 
-# 1.3 ---scale training data both X and y
+# 1.3 ---scale training and testing data
 scaler = MinMaxScaler(feature_range=(0, 1))
 scaledXYtrain = scaler.fit_transform(XYtrain)
 scaledXYtest = scaler.transform(XYtest)
@@ -98,13 +98,11 @@ print('shape of train_X, train_y, and test_X: ', train_X.shape, train_y.shape, t
 # print('min and max of train_y', np.min(train_y,axis=0), np.max(train_y,axis=0))
 # print('min and max of test_X', np.min(test_X,axis=0), np.max(test_X,axis=0))
 
-
 # reshape input to be 3D [samples, timesteps, features]
 train_X = train_X.reshape((train_X.shape[0], ndays, ninputs))
 test_X = test_X.reshape((test_X.shape[0], ndays, ninputs))
 print('shape of train_X and test_X in 3D: ', train_X.shape, test_X.shape)
 
-# exit()
 
 # 1.3 ---define and fit LSTM model
 Nepoch = 100
@@ -119,10 +117,9 @@ NNytest =np.zeros((test_X.shape[0],Nepoch))
 
 # 1.3 ---define and fit LSTM model
 model = Sequential()
-model.add(LSTM(Nnode, input_shape=(train_X.shape[1], train_X.shape[2])))#, return_sequences=True))
-# model.add(LSTM(10))
+model.add(LSTM(Nnode, input_shape=(train_X.shape[1], train_X.shape[2])))
 model.add(Dense(nfuture)) 
-adam = optimizers.adam(lr=0.001)  #default lr=0.001
+adam = optimizers.adam(lr=0.001)  
 model.compile(loss='mse', optimizer=adam)
 
 for i in range(Nepoch):
